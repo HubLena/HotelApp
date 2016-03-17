acb680f37cf3428aa3e4ac4b340ab134f0feee32
HubLena <gelena.finberg@gmail.com>
2016-03-16 19:08:15 -0700

    Update Info.java

--- src/main/java/com/sqa/jf/util/helper/Info.java
+++ src/main/java/com/sqa/jf/util/helper/Info.java
@@ -1,11 +1,7 @@
 /**
  * File Name: Info.java<br>
- * Nepton, Jean-francois<br>
- * Java Boot Camp Exercise<br>
- * Instructor: Jean-francois Nepton<br>
- * Created: Feb 10, 2016
  */
-package com.sqa.jf.util.helper;
+package com.sqa.gf.util.helper;
 
 import java.lang.reflect.*;
 import java.util.*;
@@ -14,8 +10,6 @@ import java.util.*;
  * Create a helper class to add to your framework which gives information on any
  * class you create. The method signatures are below. Try to implement one at a
  * time. Create a test case for each method.
- *
- * @author Nepton, Jean-francois
  * @version 1.0.0
  * @since 1.0
  */
